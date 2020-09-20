767e07e089997c0de748d74e9465f62094fe3e15
SOURAV ROY <souravroy.info13@gmail.com>
2020-09-20 14:54:37 +0600

    Facebook Start💗💕

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Facebook Clone </h1>
+        {/*    Header  */}
+        {/*    App Body  */}
+        {/*    SideBar  */}
+        {/*    Feed  */}
+        {/*    Widget  */}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDSZTLeC97NJDMTRq8zwEVJY_XG_JJEEFo",
+    authDomain: "facebook-start.firebaseapp.com",
+    databaseURL: "https://facebook-start.firebaseio.com",
+    projectId: "facebook-start",
+    storageBucket: "facebook-start.appspot.com",
+    messagingSenderId: "10577863128",
+    appId: "1:10577863128:web:f8e90771660d6c745092ad",
+    measurementId: "G-KBBZ2LXZKB"
+};
