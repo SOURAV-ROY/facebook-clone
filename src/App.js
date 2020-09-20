805650b627d9fc20df6a83401538947ed1dff708
SOURAV ROY <souravroy.info13@gmail.com>
2020-09-20 16:29:45 +0600

    Header Is Awesome ✔💙

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
     return (
         <div className="App">
-            <h1>Facebook Clone </h1>
             {/*    Header  */}
             <Header/>
             {/*    App Body  */}
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Avatar, IconButton} from "@material-ui/core";
 
 function Header() {
 
@@ -15,11 +25,36 @@ function Header() {
                     <input type="text"/>
                 </div>
             </div>
-            <div className="header_middle">
+
+            <div className="header_center">
+
+                <div className="header_option">
+                    <HomeIcon fontSize="large"/>
+                </div>
+                <div className="header_option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header_option">
+                    <SubscriptionsOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header_option">
+                    <StorefrontOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header_option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
 
             </div>
-            <div className="header_right">
 
+            <div className="header_right">
+                <div className="header_info">
+                    <Avatar/>
+                    <h4>Sourav Roy</h4>
+                </div>
+                <IconButton><AddIcon/></IconButton>
+                <IconButton><ForumIcon/></IconButton>
+                <IconButton><NotificationsActiveIcon/></IconButton>
+                <IconButton><ExpandMoreIcon/></IconButton>
             </div>
         </div>
     );
