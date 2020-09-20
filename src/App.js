1c0a5fc6dff5f62eaefe61b31fe6d1ef5e5d72e0
SOURAV ROY <souravroy.info13@gmail.com>
2020-09-20 16:57:05 +0600

    SideBar Created 🙏

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
     return (
         <div className="App">
             {/*    Header  */}
             <Header/>
-            {/*    App Body  */}
-            {/*    SideBar  */}
-            {/*    Feed  */}
-            {/*    Widget  */}
+            <div className="app_body">
+                <Sidebar/>
+                {/*    App Body  */}
+                {/*    SideBar  */}
+                {/*    Feed  */}
+                {/*    Widget  */}
+            </div>
+
         </div>
     );
 }
---
+++ src/SidebarRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SidebarRow.css";
+
+function SidebarRow() {
+    return (
+        <div className="sidebarRow">
+            <p>Side Bar Row</p>
+        </div>
+    );
+}
+
+export default SidebarRow;
