cef4a7b79533973d59fb47665a9a3a5db6706775
SOURAV ROY <souravroy.info13@gmail.com>
2020-09-23 14:47:23 +0600

    Firebase firestore work Awesome 💚💚

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,16 @@ import React from 'react';
 import "./Login.css";
 import Button from '@material-ui/core/Button';
 
+import {auth, provider} from "./firebase";
+
 function Login() {
     const signIn = () => {
-
+        //Sign In ******************************************************
+        auth.signInWithPopup(provider)
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => alert(error.message))
     }
     return (
         <div className="login">
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDSZTLeC97NJDMTRq8zwEVJY_XG_JJEEFo",
     authDomain: "facebook-start.firebaseapp.com",
@@ -9,3 +11,11 @@ const firebaseConfig = {
     appId: "1:10577863128:web:f8e90771660d6c745092ad",
     measurementId: "G-KBBZ2LXZKB"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
